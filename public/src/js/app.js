44815b9f8721fbceeba593658605d1ec70067ed1
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2018-12-19 11:23:33 -0600

    ADded Service Worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,5 +2,8 @@
 if ('serviceWorker' in navigator) {
     // Register a new Service Worker
     navigator.serviceWorker
-        .register('/sw.js');
+        .register('/sw.js')
+        .then(function () {
+            console.log("Service Worker Registered");
+        });
 }
